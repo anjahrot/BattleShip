f3210634ef8affc2f94813ee6ec42547315e49f9
anjahrot <157907978+anjahrot@users.noreply.github.com>
2024-10-03 15:00:54 +0200

    <feat>Setup module which will manager the DOM, setting up the user interface.

---
+++ src/DOM_Manager.js
@@ -0,0 +1,9 @@
+function domManager(){
+
+    const boardDiv = document.querySelector(".board");
+    const boardRealPlayer = document.querySelector(".realPlayerBoard");
+    const boardComputer = document.querySelector(".computerBoard");
+
+
+
+}
