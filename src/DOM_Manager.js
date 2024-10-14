dc2259da6bfa9149d48953613236f19a5417a74a
anjahrot <157907978+anjahrot@users.noreply.github.com>
2024-10-14 14:55:46 +0200

    <feat>Added DOM module, to render boards on the screen

--- src/DOM_Manager.js
+++ src/DOM_Manager.js
@@ -1,9 +1,39 @@
-function domManager(){
+const domManager = (() => {
 
     const boardDiv = document.querySelector(".board");
     const boardRealPlayer = document.querySelector(".realPlayerBoard");
     const boardComputer = document.querySelector(".computerBoard");
 
+    //update board after turn
+    const updateBoard = (board) => {
+        //clear content before rendering updated screen
+        boardRealPlayer.textContent = '';
+        
+        //render board
+        let rowIndex = 0;
+        board.forEach(row => {
+            row.forEach((square, columnIndex) => {
+                const squareButton = document.createElement("button");
+                squareButton.classList.add('square');
+                //need row and column index to choose squares in DOM
+                squareButton.dataset.row = rowIndex;
+                squareButton.dataset.column = columnIndex;
+                
+                if(board[rowIndex][columnIndex] === 0){
+                    squareButton.style.backgroundColor = "grey";
+                }
+                else {
+                     squareButton.style.backgroundColor = "black";
+                }
+                boardRealPlayer.appendChild(squareButton);
+            })
+            rowIndex++;
+        })
+    }
 
 
-}
+    return {updateBoard}
+
+})();
+
+export default domManager;
